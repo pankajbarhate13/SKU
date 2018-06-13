03f2cdce83ad0525952870dfea571e9eaafa5de3
Pankaj <pankajbarhate13@gmail.com>
2018-06-13 17:52:22 +0530

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # get 'home/index'
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  authenticated :user do
+    root 'home#index', as: 'authenticated_root'
+  end
+
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
